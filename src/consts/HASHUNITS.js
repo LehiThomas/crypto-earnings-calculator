9408384f5992ad39ef56c148d71387685c624f36
Lehi Thomas <lehithomas25@gmail.com>
2017-12-19 20:48:33 -0600

    changed hash price

--- src/consts/HASHUNITS.js
+++ src/consts/HASHUNITS.js
@@ -4,27 +4,27 @@ export const HASHUNITS = [
 		label: 'TH/s',
 		speed: 1000000000000,
 		fee: .35,
-		USDPrice: 150.00
+		USDPrice: 220.00
 	},
 	{
 		key: "GH",
 		label: 'GH/s',
 		speed: 1000000000,
 		fee: .00035,
-		USDPrice: 0.15
+		USDPrice: 0.22
 	},
 	{
 		key: "MH",
 		label: 'MH/s',
 		speed: 1000000,
 		fee: .00000035,
-		USDPrice: 0.00015
+		USDPrice: 0.00022
 	},
 	{
 		key: "KH",
 		label: 'KH/s',
 		speed: 1000,
 		fee: .00000000035,
-		USDPrice: 0.00000015
+		USDPrice: 0.00000022
 	}
 ]
