9362ef42c1da1d1950685bd1f569629a6949d3a3
Austin Lovell <austin.d.lovell@gmail.com>
2018-01-05 00:07:49 -0700

    updated admob codes

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const config = {
-	admob: "ca-app-pub-4439436708786641~1584773595",
-	banner_id: "ca-app-pub-4439436708786641/3441367479",
+	admob: "ca-app-pub-4439436708786641~3809293866",
+	banner_id: "ca-app-pub-4439436708786641/4164517086",
 	reflink: "https://hashflare.io/r/B855B218"
 }
